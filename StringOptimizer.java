53aa817e2e09b3d8559adeac228a539a3752f902
kaifmks440 <kaifmks440@gmail.com>
2026-06-09 10:45:12 +0530

    added String DSA questions1

---
+++ StringOptimizer.java
@@ -0,0 +1,28 @@
+
+
+public class StringOptimizer
+{
+	public static void main(String[] args) {
+		
+		String s = "aabbbccdaaeeffbb";
+		String output="";
+		int count=1;
+		
+		System.out.println(s.length());
+		
+		for(int i=1;i<s.length();i++){
+		    
+		    if( s.charAt(i) == s.charAt(i-1)){
+		        count++;
+		    }else{
+		        output=output+s.charAt(i-1)+count;
+		        count=1;
+		    }
+		    
+		}
+		
+		output=output+s.charAt(s.length()-1)+count;
+		
+		System.out.println(output);
+	}
+}
